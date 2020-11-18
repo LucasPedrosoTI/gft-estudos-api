bd79c1d489e1c3ba3d7c25afb0f6f77ddd8b4a88
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2020-11-18 15:31:58 -0300

    3.7 criar categoria e buscar por ID

--- src/main/java/com/gft/estudosapi/resource/CategoriaResource.java
+++ src/main/java/com/gft/estudosapi/resource/CategoriaResource.java
@@ -1,14 +1,22 @@
 package com.gft.estudosapi.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import com.gft.estudosapi.model.Categoria;
 import com.gft.estudosapi.repository.Categorias;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categorias")
@@ -21,4 +29,24 @@ public class CategoriaResource {
   public List<Categoria> listar() {
     return categorias.findAll();
   }
+
+  @PostMapping
+  public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+    Categoria categoriaSalva = categorias.save(categoria);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(categoriaSalva.getId())
+        .toUri();
+
+    response.setHeader("Location", uri.toASCIIString());
+
+    return ResponseEntity.created(uri).body(categoriaSalva);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+    Categoria categoria = categorias.findById(id).orElse(null);
+
+    return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+  }
+
 }
