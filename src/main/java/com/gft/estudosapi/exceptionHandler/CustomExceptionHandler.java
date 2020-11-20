bb523881228bf14402ec061f5ac3b78edbee9101
lspt <lucas.pedroso@gft.com>
2020-11-20 15:32:30 -0300

    5.4 - validando inconsistências

--- src/main/java/com/gft/estudosapi/exceptionHandler/CustomExceptionHandler.java
+++ src/main/java/com/gft/estudosapi/exceptionHandler/CustomExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -25,17 +28,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
 
 	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 
-		String mensagemUsuario = this.messageSource.getMessage("mensagem.invalida", null,
-				LocaleContextHolder.getLocale());
+		String mensagemUsuario = this.messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 
 		Optional<Throwable> cause = Optional.ofNullable(ex.getCause());
 
@@ -47,15 +49,15 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 
 		List<Erro> erros = this.criarListaDeErros(ex.getBindingResult());
 
 		return this.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
-	@org.springframework.web.bind.annotation.ExceptionHandler({ EmptyResultDataAccessException.class })
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
 			WebRequest request) {
@@ -66,6 +68,18 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDev));
 
 		return this.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+			WebRequest request) {
+		String mensagemUsuario = this.messageSource.getMessage("recurso.operacao-nao-permitida", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDev = ExceptionUtils.getRootCauseMessage(ex);
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDev));
+
+		return this.handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
--- src/main/java/com/gft/estudosapi/resource/LancamentoResource.java
+++ src/main/java/com/gft/estudosapi/resource/LancamentoResource.java
@@ -2,13 +2,22 @@ package com.gft.estudosapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.gft.estudosapi.event.RecursoCriadoEvent;
 import com.gft.estudosapi.model.Lancamento;
 import com.gft.estudosapi.repository.Lancamentos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +27,9 @@ public class LancamentoResource {
 
   @Autowired
   Lancamentos lancamentos;
+
+  @Autowired
+  private ApplicationEventPublisher publisher;
 
   @GetMapping
   public List<Lancamento> listarLancamentos() {
@@ -29,5 +41,14 @@ public class LancamentoResource {
     return lancamentos.findById(id).orElseThrow(() -> {
       throw new EmptyResultDataAccessException(1);
     });
+  }
+
+  @PostMapping
+  public ResponseEntity<Lancamento> criar(@RequestBody @Valid Lancamento lancamento, HttpServletResponse response) {
+    Lancamento lancamentoSalvo = lancamentos.save(lancamento);
+
+    publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getId()));
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
   }
 }
